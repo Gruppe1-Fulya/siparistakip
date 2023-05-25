86671692b12967d082346e9f648b4f3d40c747c6
narkolepsizm <aydinyasin25@gmail.com>
2023-05-25 19:07:04 +0300

    JUnit testleri her bir table için ayrı ayrı eklendi.

--- SiparisTakip/src/main/java/javamudavimleri/siparistakip/spring/veritabani/test/Testler.java
+++ SiparisTakip/src/main/java/javamudavimleri/siparistakip/spring/veritabani/test/Testler.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.HexFormat;
+
 import org.junit.jupiter.api.Test;
 
 import javamudavimleri.siparistakip.spring.veritabani.*;
@@ -13,8 +17,6 @@ import javamudavimleri.siparistakip.spring.veritabani.db.*;
 
 @DataJpaTest
 public class Testler {
-	private String masterKey = "JavaMudavimleri";
-	
 	@Autowired
     private UrunTuruDB urunturudb;
 	@Autowired
@@ -31,14 +33,88 @@ public class Testler {
     private OdemeYontemiDB odemeyontemidb;
 	@Autowired
     private OdemeDB odemedb;
-	public Testler() {}
+	
+	@Test
+	public void izinSeviyeTest() {
+		String izinSeviyeAdi = "IzinSeviyeTest";
+		int izinSeviyesi = 1;
+		izinseviyedb.save(new IzinSeviye(izinSeviyeAdi, izinSeviyesi));
+		assertEquals(izinSeviyesi, izinseviyedb.findByIzinSeviyeAdi(izinSeviyeAdi).getIzinSeviyesi());
+	}
 	
 	@Test
 	public void personelTest() {
-		long sayi = personeldb.count();
-		System.out.println("onceki sayi: "+sayi);
-		personeldb.save(new Personel("test", 2, "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"));
-		System.out.println("simdiki sayi: "+personeldb.count());
-		assertEquals(sayi+1, personeldb.count());
+		String personelAdi = "PersonelTest";
+		String sifre = "1234";
+		long izinSeviyeID = 2L;
+		HexFormat hf = HexFormat.of();
+		String sifreHashed = "basarisiz";
+		try {
+			sifreHashed =  hf.formatHex(MessageDigest.getInstance("SHA-256").digest(sifre.getBytes(StandardCharsets.UTF_8)));
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(sifreHashed == "basarisiz") {
+			assertEquals("sifreHashed", "basarisiz");
+		}
+		else {
+			personeldb.save(new Personel(personelAdi, izinSeviyeID, sifreHashed));
+			assertEquals(personelAdi, personeldb.findByPersonelSifreHashed(sifreHashed).getPersonelAdi());
+		}
 	}
+	
+	@Test
+	public void urunTuruTest() {
+		String urunTuruAdi = "UrunTuruTest";
+		urunturudb.save(new UrunTuru(urunTuruAdi));
+		assertEquals(urunTuruAdi, urunturudb.findByUrunTuruAdi(urunTuruAdi).getUrunTuruAdi());
+	}
+	
+	@Test
+	public void urunTest() {
+		String urunAdi = "UrunTest";
+		double urunFiyati = 1.25;
+		long urunTuruID = 2L;
+		urundb.save(new Urun(urunAdi, urunFiyati, urunTuruID));
+		assertEquals(urunTuruID, urundb.findByUrunTuruID(urunTuruID).get(0).getUrunTuruID());
+	}
+	
+	@Test
+	public void siparisTest() {
+		long personelID = 1L;
+		long siparisZamani = System.currentTimeMillis();
+		String masaAdi = "MasaTest";
+		siparisdb.save(new Siparis(personelID, siparisZamani, masaAdi));
+		assertEquals(1, siparisdb.count());
+	}
+	
+	@Test
+	public void masaTest() {
+		String masaAdi = "MasaTest";
+		long siparisID = 163L;
+		masadb.save(new Masa(masaAdi, siparisID));
+		assertEquals(masaAdi, masadb.findByMasaAdi(masaAdi).getMasaAdi());
+	}
+	
+	@Test
+	public void odemeYontemiTest() {
+		String odemeYontemiAdi = "OdemeYontemiTest";
+		odemeyontemidb.save(new OdemeYontemi(odemeYontemiAdi));
+		assertEquals(odemeYontemiAdi, odemeyontemidb.findByOdemeYontemiAdi(odemeYontemiAdi).getOdemeYontemiAdi());
+	}
+	
+	@Test
+	public void odemeTest() {
+		long siparisID = 163L;
+		long odemeYontemiID = 1L;
+		long personelID = 1L;
+		double bahsis = 0.75;
+		double araToplam =  75.00;
+		String odemeNotu = "OdemeNotuTest";
+		long odemeZamani = System.currentTimeMillis();
+		odemedb.save(new Odeme(siparisID, odemeYontemiID, personelID, bahsis, araToplam, odemeNotu, odemeZamani));
+		assertEquals(1, odemedb.count());
+	}
+	
 }
